e9cad65e3c3fb1bc446aae718e0baad4bc661e0c
Vivekanand-Yadav7 <Vivekanand_Yadav@srmap.edu.in>
2026-01-04 02:12:56 +0530

    cinfiguered vite

--- MyChromExtension/chrome_extension/vite.config.js
+++ MyChromExtension/chrome_extension/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'popup.html'),
+        content: resolve(__dirname, 'src/content/index.jsx'),
+        background: resolve(__dirname, 'src/background/background.js'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
+});
